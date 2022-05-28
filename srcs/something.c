e29021a37e340845317f0fd5e01bf7f351414e8f
Henrique Tomas <henriquetomas@MBP-de-Henrique.lan>
2022-05-28 16:37:26 +0100

    another commit

--- srcs/something.c
+++ srcs/something.c
@@ -10,7 +10,7 @@ int main()
 	if(!tutorial.win_ptr)
 		return (2);
 	image = new_img(300, 300, tutorial);
-	draw_square((t_square){0, 0, 300, 0x00FF00}, image);
+	draw_square((t_square){100, 100, 75, 0x00FF00}, image);
 	mlx_put_image_to_window (image.win.mlx_ptr, image.win.win_ptr, image.img_ptr, 0, 0);
 	mlx_hook(tutorial.win_ptr, 17, 0, exit_program, &tutorial);
 	mlx_loop(tutorial.mlx_ptr);
